eb74682dd2c60227e8e47bcd3500e36c158255f5
Reece <reece@steadlane.com.au>
2017-02-03 09:51:44 +1000

    Test fix++

--- tests/DBTextParserExtensionTest.php
+++ tests/DBTextParserExtensionTest.php
@@ -17,15 +17,18 @@ class DBHTMLTextTest extends SapphireTest {
 	public function testParse() {
 		// Test parse
 		/** @var DBHTMLText $obj */
-		$obj = DBField::create_field(
+		$html = DBField::create_field(
 			'HTMLText',
 			'<p>[b]Some content[/b] [test_shortcode] with shortcode</p>'
 		);
 
+		/** @var BBCodeParser $obj */
+		$obj = BBCodeParser::create($html->RAW());
+
 		// BBCode strips HTML and applies own formatting
 		$this->assertEquals(
 			'<strong>Some content</strong> shortcode content with shortcode',
-			$obj->Parse(BBCodeParser::class)->forTemplate()
+            $obj->parse()->forTemplate()
 		);
 	}
 }
